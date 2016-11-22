a9fadb18f1c971f941f63374aca1786feb6fdb7f
Alexandre PONCET <alexandreponcet.mail@gmail.com>
2016-11-22 23:54:15 +0100

    Fix redux form

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS='fetch_posts';
+export const CREATE_POST='create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=iabdioabndoandoangermanium';
@@ -10,6 +11,15 @@ export function fetchPost() {
     
     return {
         type: FETCH_POSTS,
+        payload: request
+    };
+}
+
+export function createPost(props) {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+    return {
+        type: CREATE_POST,
         payload: request
     };
 }
--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostNew extends Component {
     render() {
-        const { fields: { titre, categories, contenu }, handleSubmit } = this.props;
+        const { fields: { title, categories, content }, handleSubmit } = this.props;
        
         return (
-            <form onSubmit={handleSubmit} >
+            <form onSubmit={handleSubmit(this.props.createPost)} >
                 <h3>Créer un article</h3>
                 <div className="form-group">
                     <label>Titre</label>
-                    <input type="text" className="form-control" {...titre} />
+                    <input type="text" className="form-control" {...title} />
                 </div>
                  <div className="form-group">
                     <label>Catégories</label>
@@ -18,7 +19,7 @@ class PostNew extends Component {
                 </div>
                  <div className="form-group">
                     <label>Contenu</label>
-                    <textarea className="form-control" {...contenu} />
+                    <textarea className="form-control" {...content} />
                 </div>
 
                 <button type="submit" className="btn btn-primary">Envoyer</button>
@@ -27,7 +28,10 @@ class PostNew extends Component {
     }
 }
 
+//connect : first argument is mapStateToProps 
+//second : mapDispatchToProps 
+//redux Form : 1st is form config , and same argument than connect
 export default reduxForm({
     form: 'PostNew',
-    fields: ['titre', 'categories', 'contenu']
-})(PostNew);
+    fields: ['title', 'categories', 'content']
+},null,{createPost})(PostNew);
